9f95ba33f1e4dae61f77a325d825c78967f50a2b
Abdallah Lisser BTS SIO LLA <144156904+abdallahlisserbtssiolla@users.noreply.github.com>
2024-03-25 11:30:41 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+wget --no-cache -O /tmp/INSTALL.sh https://raw.githubusercontent.com/MISP/MISP/2.4/INSTALL/INSTALL.sh
+bash /tmp/INSTALL.sh
+
+wget --no-cache -O /tmp/INSTALL.sh https://raw.githubusercontent.com/MISP/MISP/2.4/INSTALL/INSTALL.sh
+bash /tmp/INSTALL.sh -A
